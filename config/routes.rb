cbdbbbd2e29fb9dbeb76a6883714fcec45fa1606
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-26 00:38:16 +0300

    Implement Landing Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
+
+  root 'landing#index'
+
   # Defines the root path route ("/")
   # root "articles#index"
   resources :groups do
