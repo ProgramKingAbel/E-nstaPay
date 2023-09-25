202bf19a7863cd5d6df79bc445bd794af0f8a0b9
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-25 18:03:28 +0300

    Add FK to categories for user_id

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,25 @@
 class GroupsController < ApplicationController
+  load_and_authorize_resource
   def index
+    @groups = current_user.groups.includes(:payments)
   end
 
   def show
+    @group = Group.includes(:payments).find(params[:id])
   end
 
   def new
   end
 
   def create
+    @group = current_user.groups.build(group_params)
+
+    if @group.save
+      redirect_to groups_path, notice: 'Group was successfully created.'
+    else
+      flash.now[:alert] = 'Group could not be created.'
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +30,10 @@ class GroupsController < ApplicationController
 
   def destroy
   end
+end
+
+private
+
+def group_params
+  params.require(:group).permit(:name, :icon)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :groups do
     resources :payments
   end
+
+ 
 end
