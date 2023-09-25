19cf3c62eb9b8c54eca95e3db011b710146e7e83
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-25 14:37:16 +0300

    Define associations between models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_104941) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_112613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_104941) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "groups_payments", id: false, force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "payment_id", null: false
   end
 
   create_table "payments", force: :cascade do |t|
