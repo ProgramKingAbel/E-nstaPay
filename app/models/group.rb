8bdb4415dc750b68f88d1f2c435acd795b95004b
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-25 14:50:03 +0300

    Configure linters

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
-    belongs_to :user
-    has_and_belongs_to_many :payments
+  belongs_to :user
+  has_and_belongs_to_many :payments
 
-    validates :name, presence: true
-    validates :icon, presence: true
+  validates :name, presence: true
+  validates :icon, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :payments, foreign_key: 'author_id'
-    has_many :groups
+  has_many :payments, foreign_key: 'author_id'
+  has_many :groups
 
-    validates :name, presence: true
+  validates :name, presence: true
 end
