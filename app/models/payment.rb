834e8374dd9d2195925a6c2cfa43b8ac9c7ced35
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-29 18:32:31 +0300

    fix bug

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,6 +5,7 @@ class Payment < ApplicationRecord
   validates :author_id, presence: true
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates_presence_of :group_ids, message: 'Please select at least one category'
 
   attribute :amount, :decimal, default: 0
 end
