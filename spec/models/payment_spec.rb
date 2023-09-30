d50ba00b94f2a4f05a435e380ca546f18d29c810
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-30 05:32:27 +0300

    Add fixed green button in index views

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe Payment, type: :model do
   it 'is valid with a user, name, and non-negative amount' do
     user = User.create(name: 'John Doe', email: 'john@example.com', password: 'password')
-    payment = Payment.new(user:, name: 'Test Payment', amount: 10.50)
+    group = Group.create(name: 'Group 1', icon: 'https://cdn-icons-png.flaticon.com/512/5977/5977588.png', user:)
+    payment = Payment.new(user:, name: 'Test Payment', amount: 10.50, groups: [group])
     expect(payment).to be_valid
   end
 
   it 'is invalid without a user' do
-    payment = Payment.new(name: 'Test Payment', amount: 10.50)
+    user = User.create(name: 'John Doe', email: 'john@example.com', password: 'password')
+    group = Group.create(name: 'Group 1', icon: 'https://cdn-icons-png.flaticon.com/512/5977/5977588.png', user:)
+    payment = Payment.new(name: 'Test Payment', amount: 10.50, groups: [group])
     expect(payment).not_to be_valid
   end
 
@@ -26,7 +29,8 @@ RSpec.describe Payment, type: :model do
 
   it 'is valid with a zero amount' do
     user = User.create(name: 'John Doe', email: 'john@example.com', password: 'password')
-    payment = Payment.new(user:, name: 'Test Payment', amount: 0.00)
+    group = Group.create(name: 'Group 1', icon: 'https://cdn-icons-png.flaticon.com/512/5977/5977588.png', user:)
+    payment = Payment.new(user:, name: 'Test Payment', amount: 0.00, groups: [group])
     expect(payment).to be_valid
   end
 end
